027b5e0c85d76a5a4130adab71b1622f6cb44c6b
Samuel Ricardo P. Galvão <104034355+samuelgalvao@users.noreply.github.com>
2023-03-07 17:00:47 -0300

    some upgrades

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const url = 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/all'
 
 
-fetch(url, {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '333edf6b51msh4fc2df57083d96dp1be044jsna95ce001ba0e',
-    'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
-  }
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+
+function getData(url) {
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '333edf6b51msh4fc2df57083d96dp1be044jsna95ce001ba0e',
+      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.error(error));
+}
+function handleData(data) {
+  var num = Math.floor(Math.random() * 1200) 
+  const randomKanji = data[num]
+  const kanjiObj = randomKanji.kanji
+  const kanjiImage = randomKanji.radical.image
+  const examples = randomKanji.examples
+  const meaning = kanjiObj.meaning
+  const romaji = kanjiObj.onyomi.romaji
+  const katakana = kanjiObj.onyomi.katakana
+  console.log(katakana)
+}
+
+getData(url).then(handleData);
 
 function flipCard(card){
+  
   card.classList.toggle('flipped')
 }
