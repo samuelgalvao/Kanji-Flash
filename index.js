29f7e9617f0636a4e95567b304fce47023a9a033
Samuel Ricardo P. Galvão <104034355+samuelgalvao@users.noreply.github.com>
2023-03-10 17:17:00 -0300

    new functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const url = 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/all'
+const Kanjis = []
 
 
 
@@ -19,17 +20,21 @@ function getData(url) {
 function handleData(data) {
   var num = Math.floor(Math.random() * 1200) 
   const randomKanji = data[num]
-  const kanjiObj = randomKanji.kanji
-  const imgArr = randomKanji.kanji.strokes.images
-  const kanjiImage = randomKanji.kanji.video.poster
-  const examples = randomKanji.examples
+  Kanjis.push(randomKanji)
+  executeData(randomKanji)
+}
+function executeData(data){
+  const kanjiObj = data.kanji
+  const imgArr = data.kanji.strokes.images
+  const kanjiImage = data.kanji.video.poster
+  const examples = data.examples
   const meaning = kanjiObj.meaning.english
   const romaji = kanjiObj.onyomi.romaji
   const katakana = kanjiObj.onyomi.katakana
-  console.log(randomKanji)
   addImage(kanjiImage)
   addExamples(examples)
   addMeaning(meaning, romaji, katakana)
+  addButtons(Kanjis)
 }
 
 getData(url).then(handleData);
@@ -38,13 +43,37 @@ function flipCard(card){
   
   card.classList.toggle('flipped')
 }
+function addButtons(arr) {
+
+  const buttonsPlace = document.querySelector('.buttonsPlace');
+  buttonsPlace = null
+  
+  const backButton = document.createElement('div');
+  backButton.classList.add('button', 'back');
+  backButton.textContent = 'BACK';
+  backButton.onclick = backCard; // assign the backCard function as the onclick handler
+  
+  const nextButton = document.createElement('div');
+  nextButton.classList.add('button', 'next');
+  nextButton.textContent = 'NEXT';
+  nextButton.onclick = nextCard; // assign the nextCard function as the onclick handler
+
+  buttonsPlace.appendChild(backButton);
+  buttonsPlace.appendChild(nextButton);
+}
+
 function addImage(image){
   const place = document.getElementById('kanjiImage')
   place.src = image
 }
 function addExamples(examples){
   const examplesPlace = document.querySelector('.examplesPlace')
+  examplesPlace.replaceChildren()
 
+  const h2 = document.createElement('h2')
+  h2.innerText = "Examples"
+
+  examplesPlace.appendChild(h2)
   for(let i in examples){
     const example = document.createElement('div')
     const par = document.createElement('p')
@@ -56,6 +85,7 @@ function addExamples(examples){
 
     example.appendChild(par)
     examplesPlace.appendChild(example)
+
     
      
     const showMeaning = document.createElement('button');
@@ -85,4 +115,14 @@ function addMeaning(meaning, romaji, katakana){
   romajiPlace.innerText = `Romaji: "${romaji}"`
   const katakanaPlace = document.querySelector('#katakanaPlace')
   katakanaPlace.innerText = `Katakana: "${katakana}"`
+}
+function nextCard(){
+  getData(url).then(handleData);
+}
+function backCard(){
+  if (Kanjis.length < 2){
+    alert('ERROR')
+  }else{
+    console.log(Kanjis)
+  }
 }
